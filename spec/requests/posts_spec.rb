bda3c5429be374e6df486b9af516a4dd4d0a1968
Lordkaito <cespedesisai88@gmail.com>
2022-01-13 14:24:10 +0100

    Fix linter errors

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-  describe "GET /posts" do
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /posts' do
     it 'returns http success' do
       get '/users/index/posts'
       expect(response).to have_http_status(:success)
