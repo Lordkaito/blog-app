9189597fd90093d4035dbc5dc383a0996e34aae6
Lordkaito <cespedesisai88@gmail.com>
2022-01-13 01:25:12 +0100

    Set up the controllers and views for each one

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
+
+  root 'users#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
