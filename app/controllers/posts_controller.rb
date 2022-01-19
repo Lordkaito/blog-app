fc571463bea447b948983f70103bf1640909905c
Lordkaito <cespedesisai88@gmail.com>
2022-01-19 23:28:32 +0100

    Fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,10 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:id])
-    @posts = Post.find_by_sql("SELECT * FROM posts WHERE user_id = #{params[:id]} limit 5");
+    @posts = Post.find_by_sql("SELECT * FROM posts WHERE user_id = #{params[:id]} limit 5")
   end
 
   def show
-    @posts = Post.find_by_sql("SELECT * FROM posts WHERE user_id = #{params[:id]}");
+    @posts = Post.find_by_sql("SELECT * FROM posts WHERE user_id = #{params[:id]}")
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,7 +5,6 @@ class Comment < ApplicationRecord
 
   after_save :update_comment_counter_for_a_post
 
-
   def update_comment_counter_for_a_post
     post.update(comments_counter: post.comments.count)
   end
