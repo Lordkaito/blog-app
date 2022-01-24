feed167bbc312a47dc561bf7e84e54bb8453f513
Lordkaito <cespedesisai88@gmail.com>
2022-01-24 17:11:24 +0100

    Fix sign up problems

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   has_many :posts
   has_many :comments
   has_many :likes
 
   validates :name, presence: true, length: { maximum: 50 }
-  validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_posts_from_user
     posts.order(created_at: :desc).limit(3)
