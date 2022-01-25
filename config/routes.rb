d543bf17c89ad25636cc5cef2f94c82705097d4d
Hichami Houssam <hichami.houssam@gmail.com>
2022-01-25 11:35:05 +0100

    styling sign_in sign_up pages + logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+    root to: "devise/sessions#new"
+  end
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :new, :create, :show] do
       resources :comments, only: [:new, :create]
@@ -8,7 +11,5 @@ Rails.application.routes.draw do
     end
   end
 
-  devise_scope :user do
-    root to: "devise/sessions#new"
-  end
+  
 end
