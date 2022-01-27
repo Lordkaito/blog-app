a6f6eca78ee5c323d3e4cd520691913add294dce
Hichami Houssam <hichami.houssam@gmail.com>
2022-01-27 11:13:55 +0100

    tests for log in view

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'capybara/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -65,5 +66,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  Capybara.default_driver = :selenium_chrome
+  config.include Rails.application.routes.url_helpers
+
+  config.include Capybara::DSL
 end
