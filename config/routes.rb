c7db9c8004b805e8f3c5d18d34a5d2caf9e22b88
Hichami Houssam <hichami.houssam@gmail.com>
2022-01-28 17:55:20 +0100

    retrieving previous state of the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users,
-             controllers: {
-                 sessions: 'users/sessions',
-                 registrations: 'users/registrations'
-             }
+  devise_for :users
   devise_scope :user do get '/users/sign_out' => 'devise/sessions#destroy' end
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :new, :create, :show, :destroy] do
